f4d3846898211e8fd983321b88b3b4389c2a326f
Lucas Vaz <110433221+lucasvaz-net@users.noreply.github.com>
2023-04-03 22:16:00 -0300

    Update PythonAPI.py

--- PythonAPI.py
+++ PythonAPI.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 url1 = 'URL'
 
-# Par‚metros da consulta ‡ API
+# Par√¢metros da consulta √† API
 parametros = {
     'page': 1,
     'per_page': 30,
@@ -19,27 +19,27 @@ parametros = {
 # Cria um DataFrame vazio para armazenar os dados
 df2 = pd.DataFrame()
 
-# Loop para obter todas as p·ginas de resultados
+# Loop para obter todas as p√°ginas de resultados
 while True:
 
-    # Faz uma chamada ‡ API com os par‚metros atuais
+    # Faz uma chamada √† API com os par√¢metros atuais
     response = requests.get(url1, params=parametros)
 
-    # Verifica se a resposta contÈm os dados esperados
+    # Verifica se a resposta cont√©m os dados esperados
     if 'clients' not in response.json():
-        break  # Interrompe o loop se n„o houver mais p·ginas
+        break  # Interrompe o loop se n√£o houver mais p√°ginas
 
-    # Adiciona os dados da p·gina atual ao DataFrame
+    # Adiciona os dados da p√°gina atual ao DataFrame
     dados = response.json()['clients']
     df2 = pd.concat([df2, pd.DataFrame(dados)], ignore_index=True)
 
-    # Atualiza os par‚metros de consulta para obter a prÛxima p·gina
+    # Atualiza os par√¢metros de consulta para obter a pr√≥xima p√°gina
     parametros['page'] += 1
 
-    # Imprime o n˙mero da p·gina atual
+    # Imprime o n√∫mero da p√°gina atual
     print(f"CLIENTES INTEGRACAO - Pagina {parametros['page'] - 1} concluida")
 
-    # Interrompe o loop se j· tiver obtido todas as p·ginas
+    # Interrompe o loop se j√° tiver obtido todas as p√°ginas
     if len(dados) < parametros['per_page']:
         break
 
@@ -52,41 +52,40 @@ df2.to_json('clients.json', orient='records', force_ascii=False)
 
 
 
-# AQUI COME«O A CAPTURAR OS INVOICES
+# AQUI COME√áO A CAPTURAR OS INVOICES
 
-url = 'https://gabrielalapaunipe.app.invoicexpress.com/invoices.json'
 
-# Par‚metros da consulta ‡ API
+# Par√¢metros da consulta √† API
 parametros = {
     'page': 1,
     'per_page': 30,
-    'api_key': '637c095a49b0fef977afa0740faaaf81af84b0ee'
+    'api_key': ''
 }
 
 # Cria um DataFrame vazio para armazenar os dados
 df = pd.DataFrame()
 
-# Loop para obter todas as p·ginas de resultados
+# Loop para obter todas as p√°ginas de resultados
 while True:
 
-    # Faz uma chamada ‡ API com os par‚metros atuais
+    # Faz uma chamada √† API com os par√¢metros atuais
     response = requests.get(url, params=parametros)
 
-    # Verifica se a resposta contÈm os dados esperados
+    # Verifica se a resposta cont√©m os dados esperados
     if 'invoices' not in response.json():
-        break  # Interrompe o loop se n„o houver mais p·ginas
+        break  # Interrompe o loop se n√£o houver mais p√°ginas
 
-    # Adiciona os dados da p·gina atual ao DataFrame
+    # Adiciona os dados da p√°gina atual ao DataFrame
     dados = response.json()['invoices']
     df = pd.concat([df, pd.DataFrame(dados)], ignore_index=True)
 
-    # Atualiza os par‚metros de consulta para obter a prÛxima p·gina
+    # Atualiza os par√¢metros de consulta para obter a pr√≥xima p√°gina
     parametros['page'] += 1
 
-    # Imprime o n˙mero da p·gina atual
+    # Imprime o n√∫mero da p√°gina atual
     print(f"VENDAS INTEGRACAO - Pagina {parametros['page'] - 1} concluida")
 
-    # Interrompe o loop se j· tiver obtido todas as p·ginas
+    # Interrompe o loop se j√° tiver obtido todas as p√°ginas
     if len(dados) < parametros['per_page']:
         break
 
